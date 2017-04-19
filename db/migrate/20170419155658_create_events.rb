f4bdc498997cf7273c5516f61d05e2f3f717975a
Sean Clarke <xseanclarke@gmail.com>
2017-04-19 16:52:17 +0000

    record event removal and est activerecord model relationships

--- db/migrate/20170419155658_create_events.rb
+++
@@ -1,9 +0,0 @@
-class CreateEvents < ActiveRecord::Migration[5.0]
-  def change
-    create_table :events do |t|
-      t.references :branch, index: true, foreign_key: true
-      t.references :user, index: true, foreign_key: true
-      t.timestamps
-    end
-  end
-end

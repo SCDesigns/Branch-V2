487667055abea60b9fbdc878fd4c3362bf6117e3
Sean Clarke <xseanclarke@gmail.com>
2017-04-19 16:23:28 +0000

    fixed branches (event now name)+ category migration

--- db/migrate/20170419153551_create_branches.rb
+++ db/migrate/20170419153551_create_branches.rb
@@ -4,11 +4,12 @@ class CreateBranches < ActiveRecord::Migration[5.0]
       t.references :city, index: true, foreign_key: true
       t.references :category, index: true, foreign_key: true
       t.references :user, index: true, foreign_key: true
-      t.string :event
+      t.string :name
       t.string :organization
       t.date :date
       t.string :location
       t.text :info # note text allows for longer description here than string in prev branch app
+      t.timestamps
     end
   end
 end

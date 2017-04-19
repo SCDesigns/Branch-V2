798d4f2773f4d49d84e598e02329a779bf35eafd
Sean Clarke <xseanclarke@gmail.com>
2017-04-19 16:18:09 +0000

    developed branch migration

--- db/migrate/20170419153551_create_branches.rb
+++ db/migrate/20170419153551_create_branches.rb
@@ -1,8 +1,14 @@
 class CreateBranches < ActiveRecord::Migration[5.0]
   def change
     create_table :branches do |t|
-
-      t.timestamps
+      t.references :city, index: true, foreign_key: true
+      t.references :category, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+      t.string :event
+      t.string :organization
+      t.date :date
+      t.string :location
+      t.text :info # note text allows for longer description here than string in prev branch app
     end
   end
 end

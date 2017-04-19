e9b689743d7f9f053cd9b9cc7fe34ca842e9516c
Sean Clarke <xseanclarke@gmail.com>
2017-04-19 16:29:43 +0000

    created comments, events, and cities migration columns

--- db/migrate/20170419155243_create_comments.rb
+++ db/migrate/20170419155243_create_comments.rb
@@ -1,7 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-
+      t.references :branch, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+      t.string :title
+      t.text :content
       t.timestamps
     end
   end
--- db/migrate/20170419155658_create_events.rb
+++ db/migrate/20170419155658_create_events.rb
@@ -1,7 +1,8 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
-
+      t.references :branch, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.timestamps
     end
   end

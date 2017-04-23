512e85cf92185fbd4ca90ff1ab550e1f9211b9ee
Sean Clarke <xseanclarke@gmail.com>
2017-04-23 21:31:12 +0000

    fixed re-routing for sign-in or new user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,8 @@
 class WelcomeController < ApplicationController
   def home
-      if user_signed_in?
-          redirect_to branches_path
-      else
-          redirect_to sign_in_path
-      end
+    if current_user
+      redirect_to branches_path
+    else
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root 'welcome#home', as: 'home'
+  root 'welcome#home'
+  devise_for :users
   resources :attendees
   resources :cities
   resources :comments
   resources :categories
-  devise_for :users
   resources :branches
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

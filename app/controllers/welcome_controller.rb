d3afadde4dc746983033b418aa8b4afa0bdd5337
Sean Clarke <xseanclarke@gmail.com>
2017-04-23 20:20:26 +0000

    added welcome controller conditional for signed-in users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def home
+      if user_signed_in?
+          redirect_to branches_path
+      else
+          redirect_to sign_in_path
+      end
   end
 end

464cf1a4acf8b4309a189843f60009e5d7fe1657
Sean Clarke <xseanclarke@gmail.com>
2017-05-03 00:25:59 -0400

    added helpers for persisting nested data to forms

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,9 @@
 module CategoriesHelper
+  def category_select(category, branch)
+    if category
+      category.name
+    else
+      select_tag "branch[category_id]", options_from_collection_for_select(Category.all, :id, :name)
+    end
+  end
 end
--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -1,9 +1,9 @@
 module CitiesHelper
-  def city_select(city, category)
+  def city_select(city, branch)
    if city
      city.name
    else
-     select_tag "category[city_id]", options_from_collection_for_select(City.all, :id, :name)
+     select_tag "branch[city_id]", options_from_collection_for_select(City.all, :id, :name)
    end
  end
 end

496323eac304bf3d2d6638d3473bb7ebe90731e6
Sean Clarke <xseanclarke@gmail.com>
2017-05-16 13:44:56 -0400

    established modified devise routes + improved syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   root 'welcome#home'
-  # resources :attendees
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks", registrations: "registrations" }
+  resources :categories
   resources :cities do
     resources :categories, only: [:index, :show] do
       resources :branches
     end
   end
-
-  resources :categories
 end

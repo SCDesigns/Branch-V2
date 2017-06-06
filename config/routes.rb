8b41de6249757f60c4e336615db1a695a4e28e1c
Sean Clarke <xseanclarke@gmail.com>
2017-06-06 15:35:10 -0400

    added link to index view / nested comments route in branches

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   root 'welcome#home'
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks", registrations: "registrations" }
   resources :categories
+  
+#Branch-Comment Route
+  resources :branches do
+    resources :comments
+  end
+
+# Full Nested Routes
   resources :cities do
     resources :categories, only: [:index, :show] do
       resources :branches
